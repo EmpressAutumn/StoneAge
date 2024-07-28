2c76f8ff9febe546cc9af056d2462758f135b5b1
Autumn <adamhoffensetz@gmail.com>
2024-07-28 16:19:47 -0400

    Change ModLoader to Fabric

---
+++ src/main/java/com/atom596/stoneage/StoneAgeDataGenerator.java
@@ -0,0 +1,11 @@
+package com.atom596.stoneage;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StoneAgeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
